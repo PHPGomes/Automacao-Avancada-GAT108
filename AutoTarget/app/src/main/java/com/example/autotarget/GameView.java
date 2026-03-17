61eab1101d388e3bcffef82db2648c9ed66feed7
PHPGomes <pedrohpgomes@gmail.com>
2026-03-17 15:46:55 -0300

    .

--- AutoTarget/app/src/main/java/com/example/autotarget/GameView.java
+++ AutoTarget/app/src/main/java/com/example/autotarget/GameView.java
@@ -8,20 +8,28 @@ import android.view.View;
 
 public class GameView extends View {
 
-    private Paint paint;
+    private Paint paint1;
+    private Paint paint2;
+    private Paint line;
 
     public GameView(Context context) {
         super(context);
-        paint = new Paint();
+        paint1 = new Paint();
+        paint2 = new Paint();
+        line = new Paint();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        paint.setColor(Color.RED);
+        paint1.setColor(Color.RED);
+        paint2.setColor(Color.BLUE);
+        line.setColor(Color.BLACK);
 
         // Desenha um alvo simples
-        canvas.drawCircle(500, 500, 50, paint);
+        canvas.drawCircle(500, 500, 50, paint1);
+        canvas.drawCircle(500,100,100,paint2);
+        canvas.drawLine(0,1200,1080,1200,line);
     }
 }
